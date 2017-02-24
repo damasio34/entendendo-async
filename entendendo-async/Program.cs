a479712518b526c700760db3fa50985e66cac999
Darlan Silva Admin <darlan.silva.s@invest.int.br>
2017-02-24 14:23:35 -0300

    Commit inicial

--- entendendo-async/Program.cs
+++ entendendo-async/Program.cs
@@ -9,7 +9,36 @@ namespace entendendo_async
     class Program
     {
         static void Main(string[] args)
+        {            
+            Console.WriteLine("************** Iniciando a console  **************");
+            var servico = new Service("Bolsa");
+            servico.OnMudancaNoStatusDoServicoEventHandler += Servico_OnMudancaNoStatusDoServicoEventHandler;
+            servico.OnNovaSoma += Servico_OnNovaSoma;
+            servico.Inicializa();
+
+            var servico2 = new Service("Fundo");
+            servico2.OnMudancaNoStatusDoServicoEventHandler += Servico_OnMudancaNoStatusDoServicoEventHandler;
+            servico2.OnNovaSoma += Servico_OnNovaSoma;
+            servico2.Inicializa();
+
+            var servico3 = new Service("Fixa ");
+            servico3.OnMudancaNoStatusDoServicoEventHandler += Servico_OnMudancaNoStatusDoServicoEventHandler;
+            servico3.OnNovaSoma += Servico_OnNovaSoma;
+            servico3.Inicializa();
+
+            Console.WriteLine("NÃO TRAVOU");
+
+            Console.ReadKey();
+        }
+
+        private static void Servico_OnNovaSoma(Service servico, int ordem, int a, int b, int delay)
         {
+            Console.WriteLine($"{servico.Nome} |{ordem}º - {a} + {b} = { a + b } | {delay}");
+        }
+        private static void Servico_OnMudancaNoStatusDoServicoEventHandler(Service servico, StatusDoServico status)
+        {            
+            Console.WriteLine($"{servico.Nome} | Mudança de status do serviço: {status}.");
+            if (status.Equals(StatusDoServico.Inicializado)) servico.GeraVariasTasks();
         }
     }
 }
---
+++ entendendo-async/Service.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace entendendo_async
+{
+    public class Service
+    {
+        public Service(string nome) { Nome = nome; }
+
+        public delegate void MudancaDeStatusDoServico(Service service, StatusDoServico statusDoServico);
+        public event MudancaDeStatusDoServico OnMudancaNoStatusDoServicoEventHandler;
+
+        public delegate void NovaSoma(Service service, int ordem, int a, int b, int delay);
+        public event NovaSoma OnNovaSoma;
+        public string Nome { get; }
+
+        public Task Inicializa()
+        {
+            this.OnMudancaNoStatusDoServicoEventHandler?.Invoke(this, StatusDoServico.Inicializando);
+
+            // Cria a task e já inicializa a mesma
+            return Task.Factory.StartNew(() =>
+            {
+                var r = new Random();
+                Thread.Sleep(r.Next(1000, 9000));
+                this.OnMudancaNoStatusDoServicoEventHandler?.Invoke(this, StatusDoServico.Inicializado);
+            });            
+        }
+        public Task GeraVariasTasks()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var r = new Random();
+
+                for (var i = 1; i <= 100; i++)
+                {
+                    Soma(i, r.Next(1, 9), r.Next(1, 9), r.Next(1000, 5000));
+                }
+            });
+        }
+
+        private Task Soma(int ordem, int a, int b, int delay)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(delay);
+                this.OnNovaSoma?.Invoke(this, ordem, a, b, delay);
+            });
+        }
+    }
+
+    public enum StatusDoServico
+    {
+        Inicializando,
+        Inicializado        
+    }
+}
